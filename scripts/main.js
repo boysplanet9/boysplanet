c20ce8918145f3d6201a1746e3fa24faf9a378b6
boysplanet9 <121615868+boysplanet9@users.noreply.github.com>
2022-12-30 19:06:56 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -390,7 +390,7 @@ function removeRankedTrainee(trainee) {
   return false;
 }
 
-const currentURL = "https://sakuraimiu.github.io/PlanetGirls999/";
+const currentURL = "https://boysplanet9.github.io/boysplanet/";
 // Serializes the ranking into a string and appends that to the current URL
 function generateShareLink() {
   let shareCode = ranking.map(function (trainee) {
